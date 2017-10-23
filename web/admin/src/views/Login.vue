99425a5ff2bd414ededba8977292a650b3971b16
fmagusta <fmagusta@tigaserangkai.co.id>
2017-10-23 16:18:33 +0700

    -

--- web/admin/src/views/Login.vue
+++ web/admin/src/views/Login.vue
@@ -1,17 +1,21 @@
 <template>
-	<el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-	<h3 class="title">KUMO Login</h3>
-		<el-form-item prop="account">
-			<el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="Account"></el-input>
-		</el-form-item>
-		<el-form-item prop="checkPass">
-			<el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="Password"></el-input>
-		</el-form-item>
-			<el-checkbox v-model="checked" checked class="remember">Remember Me</el-checkbox>
-		<el-form-item style="width:100%;">
-			<el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">Login</el-button>
-		</el-form-item>
-	</el-form>
+	<div class="login-container">
+		<el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
+			class="login-box">
+			<img src="../assets/login.png" style="display: block;  margin: 0 auto;">
+			<h3 class="title" style="margin-top:10px">Please Login</h3>
+			<el-form-item prop="account">
+				<el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="Account"></el-input>
+			</el-form-item>
+			<el-form-item prop="checkPass">
+				<el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="Password"></el-input>
+			</el-form-item>
+				<el-checkbox v-model="checked" checked class="remember">Remember Me</el-checkbox>
+			<el-form-item style="width:100%;">
+				<el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">Login</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
 </template>
 
 <script>
@@ -82,25 +86,42 @@
 </script>
 
 <style lang="scss" scoped>
-  .login-container {
-    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    -moz-border-radius: 5px;
-    background-clip: padding-box;
-    margin: 120px auto;
-    width: 350px;
-    padding: 35px 35px 15px 35px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
+	.login-container {
+		// background:url(http://thekitemap.com/images/feedback-img.jpg) no-repeat;
+		background: #f1f1f1;
+		width: 100%;
+		height: 100%;
+		padding-top: 70px;
+		// background: #00ff00;
+	}
+	// .login-box {
+	// 	background-color:rgba(72,72,72,0.4);
+	// 	padding: 35px 35px 15px 35px;
+	// 	width: 300px;
+	// 	margin: 0px auto;
+	// 	-moz-border-radius: 7px;
+	// 	-webkit-border-radius: 7px;
+	// }
+  	.login-box {
+    	box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);
+	    -webkit-border-radius: 5px;
+	    border-radius: 5px;
+	    -moz-border-radius: 5px;
+		// background-color:rgba(72,72,72,0.4);
+	    background-clip: padding-box;
+	    margin: 0px auto;
+	    width: 300px;
+	    padding: 35px 35px 15px 35px;
+	    background: #fff;
+	    border: 1px solid #eaeaea;
+	    box-shadow: 0 0 25px #cac6c6;
     .title {
-      margin: 0px auto 40px auto;
-      text-align: center;
-      color: #505458;
+		margin: 0px auto 40px auto;
+		text-align: center;
+		color: #505458;
     }
     .remember {
-      margin: 0px 0px 35px 0px;
+		margin: 0px 0px 35px 0px;
     }
   }
 </style>
