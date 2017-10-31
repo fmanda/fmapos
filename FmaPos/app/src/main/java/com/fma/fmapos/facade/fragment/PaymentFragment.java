ffe6b8f4f7183d8e23d09ce4ca349985c58286fa
fmagusta <fmagusta@tigaserangkai.co.id>
2017-10-31 17:12:54 +0700

    -

--- FmaPos/app/src/main/java/com/fma/fmapos/facade/fragment/PaymentFragment.java
+++ FmaPos/app/src/main/java/com/fma/fmapos/facade/fragment/PaymentFragment.java
@@ -116,7 +116,7 @@ public class PaymentFragment extends Fragment{
 
     private void savePayment() {
 //        DBHelper db = new DBHelper(getActivity());
-        DBHelper db = DBHelper.getInstance(getActivity())
+        DBHelper db = DBHelper.getInstance(getActivity());
         SQLiteDatabase trans = db.getWritableDatabase();
 
         modelOrder.setStatus(1);
